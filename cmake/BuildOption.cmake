0c41bf77b508df65956871b638c1687f0f3124db
Jeremy Tan <jtanx@outlook.com>
2019-07-10 20:45:09 +1000

    s1
    s2a
    
    packages
    
    header init
    
    moreinit
    
    even more
    
    hmmmm
    
    hmmm
    
    aww yeah
    
    Fixed it
    
    Fix FTS
    
    wip
    
    tmp

---
+++ cmake/BuildOption.cmake
@@ -0,0 +1,82 @@
+# Distributed under the original FontForge BSD 3-clause license
+
+#[=======================================================================[.rst:
+BuildOption
+-----------
+
+Standardises the way in which to specify a build option. Supports any
+type that the ``set()`` function supports. Options are stored in the cache.
+
+For ``BOOL`` options, passing in a 5th parameter (after the description)
+will lead to that parameter being evaluated. If it evaluates to false,
+the build option is forced to ``OFF``.
+
+The ``AUTO`` type is for tri-state Boolean options. The ``value`` parameter
+is ignored; it will always be initialised to ``AUTO``.
+
+The ``ENUM`` type is for options that may be one of a number of defined
+values. All arguments afer the description are treated as allowed values
+of the enumeration.
+
+All other types are passed directly to ``set()``.
+#]=======================================================================]
+
+function(build_option variable type value description)
+  if(${type} STREQUAL BOOL)
+    if(${ARGC} EQUAL 4)
+      option("${variable}" "${description}" "${value}")
+    elseif(${ARGC} EQUAL 5)
+      if(${ARGV4})
+        option("${variable}" "${description}" "${value}")
+      else()
+        set("${variable}" OFF CACHE BOOL "${description}" FORCE)
+      endif()
+    else()
+        message(FATAL_ERROR "Invalid number of arguments for dependent option")
+    endif()
+  elseif(${type} STREQUAL AUTO)
+    set("${variable}" AUTO CACHE STRING "${description}")
+    set_property(CACHE "${variable}" PROPERTY STRINGS AUTO ON OFF)
+  elseif(${type} STREQUAL ENUM)
+    if(${ARGC} LESS 5)
+      message(FATAL_ERROR "Must pass at least one enum type")
+    endif()
+    set("${variable}" "${value}" CACHE STRING "${description} Valid values: ${ARGN}")
+    set_property(CACHE "${variable}" PROPERTY STRINGS ${ARGN})
+    if(NOT "${${variable}}" IN_LIST ARGN)
+      message(FATAL_ERROR "'${${variable}}' is not a valid value for ${variable}, expect one of ${ARGN}")
+    endif()
+  else()
+    set("${variable}" "${value}" CACHE "${type}" "${description}")
+  endif()
+
+  if(NOT "${variable}" IN_LIST BUILD_OPTIONS)
+    set(BUILD_OPTIONS "${BUILD_OPTIONS};${variable}" CACHE INTERNAL "List of build options")
+  endif()
+endfunction()
+
+function(print_build_options)
+  message(STATUS "Build options: ")
+  foreach(opt ${BUILD_OPTIONS})
+    if("${${opt}}" STREQUAL "AUTO" AND DEFINED ${opt}_RESULT)
+      if(${opt}_RESULT)
+        message(STATUS "  ${opt} = ${${opt}} => ON")
+      else()
+        message(STATUS "  ${opt} = ${${opt}} => OFF")
+      endif()
+    else()
+      message(STATUS "  ${opt} = ${${opt}}")
+    endif()
+  endforeach()
+endfunction()
+
+function(set_default_build_type default_build_type)
+  # https://blog.kitware.com/cmake-and-the-default-build-type
+  if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+    set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
+      STRING "Choose the type of build." FORCE)
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+      "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+  endif()
+endfunction()
---
+++ cmake/TargetUtils.cmake
@@ -0,0 +1,70 @@
+# Distributed under the original FontForge BSD 3-clause license
+
+#[=======================================================================[.rst:
+TargetUtils
+-----------
+
+``alias_imported_target` copies an imported target of name ``src` into
+a new imported target of name ``dest``. This is used because CMake
+currently does not support aliasing imported targets.
+
+``make_object_interface`` creates an interface library from an
+object library of name ``objlib``. This interface includes the
+target objects from ``objlib``. Additional arguments passed into
+this function will be treated as libraries to link to this interface
+library. This function is for use on CMake versions less than 3.12,
+which do not support calling target_link_libraries directly on
+object libraries.
+
+``set_supported_c_compiler_flags`` checks to see if the provided list
+of flags are supported by the C compiler. Supported flags are stored
+into ``dst`` as a cache variable.
+
+#]=======================================================================]
+
+function(alias_imported_target dest src)
+  if(TARGET ${src} AND NOT TARGET ${dest})
+    add_library(${dest} INTERFACE IMPORTED)
+    foreach(prop INTERFACE_INCLUDE_DIRECTORIES INTERFACE_LINK_LIBRARIES INTERFACE_LINK_OPTIONS INTERFACE_COMPILE_OPTIONS)
+      get_property(_prop_val TARGET ${src} PROPERTY ${prop})
+      set_property(TARGET ${dest} PROPERTY ${prop} ${_prop_val})
+    endforeach()
+  endif()
+endfunction()
+
+function(make_object_interface objlib)
+  add_library(${objlib}_interface INTERFACE)
+  target_sources(${objlib}_interface INTERFACE $<TARGET_OBJECTS:${objlib}>)
+  target_link_libraries(${objlib}_interface
+    INTERFACE
+      ${ARGN}
+  )
+  target_include_directories(${objlib}
+    PUBLIC
+      $<TARGET_PROPERTY:${objlib}_interface,INTERFACE_INCLUDE_DIRECTORIES>
+  )
+endfunction()
+
+function(set_supported_c_compiler_flags dst)
+  if(NOT DEFINED "${dst}")
+    include(CheckCCompilerFlag)
+    include(CMakePushCheckState)
+    cmake_push_check_state(RESET)
+    set(CMAKE_REQUIRED_QUIET TRUE)
+
+    foreach(_arg ${ARGN})
+      message(STATUS "Checking if ${_arg} is supported...")
+      check_c_compiler_flag(${_arg} _supported_flag)
+      if(_supported_flag)
+        message(STATUS "  Flag is supported: ${_arg}")
+        list(APPEND _supported_flags ${_arg})
+      else()
+        message(STATUS "  Flag is unsupported: ${_arg}")
+      endif()
+      unset(_supported_flag CACHE)
+    endforeach()
+
+    set("${dst}" "${_supported_flags}" CACHE STRING "Supported compiler flags")
+    cmake_pop_check_state()
+  endif()
+endfunction()
