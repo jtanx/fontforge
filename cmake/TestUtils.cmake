2fc340a056e30cd64d021c6b0248bc295cc5744d
Jeremy Tan <jtanx@outlook.com>
2019-07-27 12:14:44 +1000

    simplify

--- cmake/TestUtils.cmake
+++ cmake/TestUtils.cmake
@@ -9,7 +9,10 @@ function(add_download_target font url)
   )
 endfunction()
 
-function(add_ff_test test_name test_script)
+function(_add_systest test_mode binary test_script)
+  get_filename_component(_test_name "${test_script}" NAME_WE)
+  set(_test_name "${_test_name}_${test_mode}")
+
   list(LENGTH ARGN _arglen)
   if (${_arglen} LESS 1)
     message(FATAL_ERROR "Must pass a description as the last argument")
@@ -17,14 +20,15 @@ function(add_ff_test test_name test_script)
   list(GET ARGN -1 _description)
   list(REMOVE_AT ARGN -1)
 
+  # Prior to CMake 3.9, skipped tests fail the suite
   if(${CMAKE_VERSION} VERSION_LESS "3.9")
     set(_skip_arg --skip-as-pass)
   endif()
 
-  add_test(NAME ${test_name}_ff
+  add_test(NAME ${_test_name}
     COMMAND systestdriver
-      --mode ff
-      --binary "$<TARGET_FILE:fontforgeexe>"
+      --mode ${test_mode}
+      --binary "${binary}"
       --script "${CMAKE_CURRENT_SOURCE_DIR}/${test_script}"
       --exedir "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
       --libdir "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}"
@@ -35,61 +39,23 @@ function(add_ff_test test_name test_script)
       ${ARGN}
     WORKING_DIRECTORY
       "${CMAKE_CURRENT_BINARY_DIR}"
-    )
-  set_tests_properties(${test_name}_ff PROPERTIES SKIP_RETURN_CODE 77)
+  )
+  set_tests_properties(${_test_name} PROPERTIES SKIP_RETURN_CODE 77)
 endfunction()
 
-function(add_py_test test_name test_script)
+function(add_ff_test test_script)
+  _add_systest(ff "$<TARGET_FILE:fontforgeexe>" "${test_script}" ${ARGN})
+endfunction()
+
+function(add_py_test test_script)
   list(FIND ARGN PYHOOK_DISABLED _disable_pyhook_index)
   if(NOT _disable_pyhook_index LESS 0)
     list(REMOVE_AT ARGN ${_disable_pyhook_index})
     set(_disable_pyhook 1)
   endif()
 
-  list(LENGTH ARGN _arglen)
-  if (${_arglen} LESS 1)
-    message(FATAL_ERROR "Must pass a description as the last argument")
-  endif()
-  list(GET ARGN -1 _description)
-  list(REMOVE_AT ARGN -1)
-
-  if(${CMAKE_VERSION} VERSION_LESS "3.9")
-    set(_skip_arg --skip-as-pass)
-  endif()
-
-  add_test(NAME ${test_name}_py
-    COMMAND systestdriver
-      --mode py
-      --binary "$<TARGET_FILE:fontforgeexe>"
-      --script "${CMAKE_CURRENT_SOURCE_DIR}/${test_script}"
-      --exedir "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
-      --libdir "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}"
-      --argdir "${CMAKE_CURRENT_BINARY_DIR}/fonts"
-      --argdir "${CMAKE_CURRENT_SOURCE_DIR}/fonts"
-      --desc "${_description}"
-      ${_skip_arg}
-      ${ARGN}
-    WORKING_DIRECTORY
-      "${CMAKE_CURRENT_BINARY_DIR}"
-    )
-  set_tests_properties(${test_name}_py PROPERTIES SKIP_RETURN_CODE 77)
-
+  _add_systest(py "$<TARGET_FILE:fontforgeexe>" "${test_script}" ${ARGN})
   if(ENABLE_PYTHON_EXTENSION AND NOT _disable_pyhook)
-    add_test(NAME ${test_name}_pyhook
-      COMMAND systestdriver
-        --mode pyhook
-        --binary "${Python3_EXECUTABLE}"
-        --script "${CMAKE_CURRENT_SOURCE_DIR}/${test_script}"
-        --exedir "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
-        --libdir "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}"
-        --argdir "${CMAKE_CURRENT_BINARY_DIR}/fonts"
-        --argdir "${CMAKE_CURRENT_SOURCE_DIR}/fonts"
-        --desc "${_description}"
-        ${_skip_arg}
-        ${ARGN}
-      WORKING_DIRECTORY
-        "${CMAKE_CURRENT_BINARY_DIR}"
-      )
-    set_tests_properties(${test_name}_pyhook PROPERTIES SKIP_RETURN_CODE 77)
+    _add_systest(pyhook "${Python3_EXECUTABLE}" "${test_script}" ${ARGN})
   endif()
 endfunction()
