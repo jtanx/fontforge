72419826cb1d4a50b827579e43d32a3e88ed6495
Jeremy Tan <jtanx@outlook.com>
2019-08-02 19:51:06 +1000

    native scripts

--- cmake/TargetUtils.cmake
+++ cmake/TargetUtils.cmake
@@ -20,6 +20,11 @@ object libraries.
 of flags are supported by the C compiler. Supported flags are stored
 into ``dst`` as a cache variable.
 
+``add_native_scripts`` adds the provided set of native scripts, which
+will either be installed to the binary folder on *nix platforms, having
+had the appropriate shebang prepended to the script, or installed to
+share/fontforge/nativescripts as-is on other platforms.
+
 #]=======================================================================]
 
 function(alias_imported_target dest src)
@@ -66,5 +71,32 @@ function(set_supported_c_compiler_flags dst)
 
     set("${dst}" "${_supported_flags}" CACHE STRING "Supported compiler flags")
     cmake_pop_check_state()
+  endif()
+endfunction()
+
+function(add_native_scripts)
+  if(NOT UNIX)
+    install(FILES ${ARGN} DESTINATION share/fontforge/nativescripts COMPONENT nativescripts)
+  else()
+    foreach(_script ${ARGN})
+      get_filename_component(_output "${_script}" NAME_WE)
+      set(_output "${CMAKE_CURRENT_BINARY_DIR}/${_output}")
+      add_custom_command(OUTPUT "${_output}"
+        COMMAND "${CMAKE_COMMAND}"
+          -D "INPUT:FILEPATH=${CMAKE_CURRENT_SOURCE_DIR}/${_script}"
+          -D "OUTPUT:FILEPATH=${_output}"
+          -D "HEADER:STRING=#!${CMAKE_INSTALL_PREFIX}/bin/fontforge -lang=ff"
+          -P "${CMAKE_CURRENT_SOURCE_DIR}/../cmake/scripts/AddHeader.cmake"
+        DEPENDS "${_script}" "${CMAKE_CURRENT_SOURCE_DIR}/../cmake/scripts/AddHeader.cmake"
+        VERBATIM
+      )
+      install(FILES "${_output}"
+        DESTINATION "bin"
+        PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+        COMPONENT nativescripts
+      )
+      list(APPEND _script_outputs "${_output}")
+    endforeach()
+    add_custom_target(nativescripts ALL DEPENDS "${_script_outputs}")
   endif()
 endfunction()
