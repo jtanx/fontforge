b4aef5e3098e05c347051dca3285c8f51a3c7605
Jeremy Tan <jtanx@outlook.com>
2021-04-22 20:33:14 +1000

    migrate

--- Unicode/uninamestest.c
+++ Unicode/uninamestest.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <ustring.h>
-#include "utype2.h"
+// #include "utype2.h"
 
 #include <assert.h>
 #include <glib.h>
@@ -46,7 +46,7 @@ int main() {
 }
 #endif
 
-#if 1
+#if 0
 
 int main() {
     for (int i = 0; i < 0x120000; ++i) {
@@ -61,6 +61,64 @@ int main() {
     return 0;
 }
 #endif
+
+#include <utype.h>
+
+#define BREAK_AFTER 0x1
+#define BREAK_BEFORE 0x2
+#define BREAK_NONSTART 0x4
+#define BREAK_NONEND 0x8
+static int BreakClassify(unichar_t ch) {
+    int flags = 0;
+    switch (g_unichar_break_type(ch)) {
+        case G_UNICODE_BREAK_SPACE:
+        case G_UNICODE_BREAK_HYPHEN:
+        case G_UNICODE_BREAK_AFTER:
+        case G_UNICODE_BREAK_ZERO_WIDTH_SPACE:
+            flags |= BREAK_AFTER;
+            break;
+        case G_UNICODE_BREAK_BEFORE:
+            flags |= BREAK_BEFORE;
+            break;
+        case G_UNICODE_BREAK_BEFORE_AND_AFTER:
+        case G_UNICODE_BREAK_IDEOGRAPHIC:
+            flags |= BREAK_AFTER | BREAK_BEFORE;
+            break;
+        case G_UNICODE_BREAK_NON_STARTER:
+        case G_UNICODE_BREAK_CLOSE_PUNCTUATION:
+            flags |= BREAK_NONSTART;
+            break;
+        case G_UNICODE_BREAK_NON_BREAKING_GLUE:
+            flags |= BREAK_NONSTART | BREAK_NONEND;
+            break;
+        case G_UNICODE_BREAK_OPEN_PUNCTUATION:
+        case G_UNICODE_BREAK_COMBINING_MARK:
+            flags |= BREAK_NONEND;
+            break;
+        default:
+            break;
+    }
+    return flags;
+}
+
+// This is a copy of the algorithm from the old utype.c/makeutype.c
+// See https://github.com/fontforge/fontforge/blob/a5dedb4010cd49a5fcfaeed5d188dd7942294005/Unicode/makeutype.c#L687-L706
+static int IsBreakBetweenOk(unichar_t ch1, unichar_t ch2) {
+    int b1 = BreakClassify(ch1), b2 = BreakClassify(ch2);
+    return (
+        ((b1 & BREAK_AFTER) && !(b2 & BREAK_NONSTART)) ||
+        ((b2 & BREAK_BEFORE) && !(b1 & BREAK_NONEND)) ||
+        (!isdigit(ch2) && ch1 == '/')
+    );
+}
+
+int main() {
+    for (int i = 1; i < 0x10000; ++i) {
+        if (isunicodepointassigned(i-1) && isunicodepointassigned(i)) {
+            assert(isbreakbetweenok(i-1, i) == IsBreakBetweenOk(i-1, i));
+        }
+    }
+}
 
 #if 0
 int main() {
